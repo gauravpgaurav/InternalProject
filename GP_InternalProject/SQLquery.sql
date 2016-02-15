07ed64b794538ec4fffd71049568e47254229c54
Gaurav Pant <gauravpgaurav@gmail.com>
2016-02-15 21:53:10 +0530

    Works

---
+++ GP_InternalProject/SQLquery.sql
@@ -0,0 +1,7 @@
+CREATE TABLE  gpant.employee
+   (	employeeId numeric NOT NULL, 
+	employeeName varchar(40), 
+	employeeRole varchar(30), 
+	employeePassword varchar(40), 
+	 PRIMARY KEY (employeeId)
+   );
