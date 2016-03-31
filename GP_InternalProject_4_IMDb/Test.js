cf5bfdb85aa74742bba367959a71f90db81665ec
Gaurav Pant <gauravpgaurav@gmail.com>
2016-03-31 11:16:49 +0530

    Working

--- GP_InternalProject_4_IMDb/Test.js
+++ GP_InternalProject_4_IMDb/Test.js
@@ -1,4 +1,7 @@
-//lets require/import the mongodb native drivers.
+var express = require('express');
+var app = express();
+
+// lets require/import the mongodb native drivers.
 var mongodb = require('mongodb');
 
 // We need to work with "MongoClient" interface in order to connect to a mongodb
@@ -33,10 +36,28 @@ MongoClient
 								})
 								.toArray(
 										function(err, result) {
+											var intCount = result.length;
 											if (err) {
 												console.log("Here!");
 												console.log(err);
 											} else if (result.length) {
+												if (intCount > 0) {
+													var strJson = "";
+													var arrSongs = "";
+													for (var i = 0; i < intCount;) {
+														arrSongs = result[i].soundTrack;
+														strJson += '{"soundTrack":"'
+																+ result[i].soundTrack
+																+ '"}'
+														i = i + 1;
+														if (i < intCount) {
+															strJson += ',';
+														}
+													}
+
+												}
+												console
+														.log(arrSongs[1].songName);
 												console.log('Found:', result);
 											} else {
 												console
@@ -49,3 +70,42 @@ MongoClient
 					}
 
 				});
+
+app.get('/', function(req, res) {
+	res.sendfile('./views/index.html');
+});
+
+app.get("/MovieDetails", function(req, res) {
+
+	MongoClient.connect(url, function(err, db) {
+
+		var cursor = db.collection('moviecol').find().toArray(
+				function(err, docs) {
+					res.send(docs);
+				});
+
+	});
+});
+
+app.get('/ShowMovie/:id', function(req, res) {
+
+	console.log("Finding Movie");
+
+	MongoClient.connect(url, function(err, db) {
+
+		db.collection('moviecol').findOne({
+			_id : parseInt(req.params.id)
+		}, function(err, docs) {
+			res.send(docs);
+		});
+	});
+});
+
+var server = app.listen(8083, function() {
+
+	var host = server.address().address;
+	var port = server.address().port;
+
+	console.log("Example app listening at http://%s:%s", host, port);
+
+});
