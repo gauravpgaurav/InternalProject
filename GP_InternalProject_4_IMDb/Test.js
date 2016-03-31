8c258037d1fc02f5d27ea94f9b259dd9ea99f340
Gaurav Pant <gauravpgaurav@gmail.com>
2016-03-31 16:56:01 +0530

    Finish

--- GP_InternalProject_4_IMDb/Test.js
+++ GP_InternalProject_4_IMDb/Test.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-
+app.use(express.static('public'));
 // lets require/import the mongodb native drivers.
 var mongodb = require('mongodb');
 
@@ -10,67 +10,7 @@ var MongoClient = mongodb.MongoClient;
 
 // Connection URL. This is where your mongodb server is running.
 var url = 'mongodb://localhost:27017/internalDb4';
-/*
-// Use connect method to connect to the Server
-MongoClient
-		.connect(
-				url,
-				function(err, db) {
-					if (err) {
-						console
-								.log(
-										'Unable to connect to the mongoDB server. Error:',
-										err);
-					} else {
-						// HURRAY!! We are connected. :)
-						console.log('Connection established to', url);
 
-						// do some work here with the database.
-						// Get the documents collection
-						var collection = db.collection('moviecol');
-
-						// Insert some users
-						collection
-								.find({
-									_id : 101
-								})
-								.toArray(
-										function(err, result) {
-											var intCount = result.length;
-											if (err) {
-												console.log("Here!");
-												console.log(err);
-											} else if (result.length) {
-												if (intCount > 0) {
-													var strJson = "";
-													var arrSongs = "";
-													for (var i = 0; i < intCount;) {
-														arrSongs = result[i].soundTrack;
-														strJson += '{"soundTrack":"'
-																+ result[i].soundTrack
-																+ '"}'
-														i = i + 1;
-														if (i < intCount) {
-															strJson += ',';
-														}
-													}
-
-												}
-												console
-														.log(arrSongs[1].songName);
-												console.log('Found:', result);
-											} else {
-												console
-														.log('No document(s) found with defined "find" criteria!');
-											}
-											// Close connection
-											db.close();
-										});
-
-					}
-
-				});
-*/
 app.get('/', function(req, res) {
 	res.sendfile('./views/index.html');
 });
